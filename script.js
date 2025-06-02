c19b2d0d224258877dc641fc67473b79c9aed569
vitorf2025 <vitor.franca21@escola.pr.gov.br>
2025-06-02 11:24:57 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ocument.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
  
